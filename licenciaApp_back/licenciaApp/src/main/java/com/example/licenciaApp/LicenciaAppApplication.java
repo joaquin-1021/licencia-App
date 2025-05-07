7b9139b13f58da06d0d762c4d6994ff6d7678c94
joaquin-1021 <joaqinn.1021@gmail.com>
2025-05-07 16:29:59 -0300

    inicio

---
+++ licenciaApp_back/licenciaApp/src/main/java/com/example/licenciaApp/LicenciaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.licenciaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenciaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenciaAppApplication.class, args);
+	}
+
+}
---
+++ licenciaApp_back/licenciaApp/src/test/java/com/example/licenciaApp/LicenciaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.licenciaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenciaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
