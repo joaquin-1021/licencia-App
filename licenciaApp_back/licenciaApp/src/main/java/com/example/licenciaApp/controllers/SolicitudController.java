0e87c0cd4d75a48b72afa68f1bf263d60d3114a0
Tomas Haulet <tomshaulet@gmail.com>
2025-06-04 16:21:47 -0300

    innicial2

--- licenciaApp_back/licenciaApp/src/main/java/com/example/licenciaApp/controllers/SolicitudController.java
+++ licenciaApp_back/licenciaApp/src/main/java/com/example/licenciaApp/controllers/SolicitudController.java
@@ -3,13 +3,11 @@ package com.example.licenciaApp.controllers;
 import com.example.licenciaApp.models.Solicitud;
 import com.example.licenciaApp.services.SolicitudService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/solicitud")
+@CrossOrigin(origins = "*")
 public class SolicitudController {
 
     @Autowired
--- licenciaApp_back/licenciaApp/src/main/java/com/example/licenciaApp/controllers/TitularController.java
+++ licenciaApp_back/licenciaApp/src/main/java/com/example/licenciaApp/controllers/TitularController.java
@@ -3,13 +3,11 @@ package com.example.licenciaApp.controllers;
 import com.example.licenciaApp.models.Titular;
 import com.example.licenciaApp.services.TitularService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/titular")
+@CrossOrigin(origins = "*")
 public class TitularController {
 
     @Autowired
